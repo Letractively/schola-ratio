15dc8d916958d58dd542458be6a52f769738228e
osnircunha <osnircunha@gmail.com>
2012-08-08 23:12:17 +0000

    versão inicial

---
+++ schola-ratio-api/src/main/java/br/facet/tcc/controle/GestaoConfiguracao.java
@@ -0,0 +1,36 @@
+/*
+ * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
+ * 
+ * Copyright (c) 2012 All rights reserved.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system, nor translated in any human or computer
+ * language in any way for any purposes whatsoever without the prior written
+ * 
+ * 
+ * (Code Template Version: 1.0)
+ */
+package br.facet.tcc.controle;
+
+import java.util.List;
+
+import br.facet.tcc.pojo.Instituicao;
+
+/**
+ * @author Osnir F CUNHA
+ * 
+ * @version TODO: class_version
+ * @since TODO: package_version
+ */
+public interface GestaoConfiguracao {
+
+    public void atualizarDestalhesInstituicao(Instituicao instituicao);
+
+    public void salvarInstituicao(Instituicao instituicao);
+
+    public void removerInstituicao(Instituicao instituicao);
+
+    public List<Instituicao> listarInstituicao();
+
+}
