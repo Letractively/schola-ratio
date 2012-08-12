8266074f423f86ff11527431c662a6018ce4c770
osnircunha@gmail.com <osnircunha@gmail.com@d23f665e-650f-5060-e4bd-094653392495>
2012-08-12 23:23:39 +0000

    initial revision

---
+++ trunk/schola-ratio-impl/src/main/java/br/facet/tcc/impl/dao/EnderecoDaoImpl.java
@@ -0,0 +1,82 @@
+/*
+ * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
+ * 
+ * Copyright (c) 2012 All rights reserved.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system, nor translated in any human or computer
+ * language in any way for any purposes whatsoever without the prior written
+ * 
+ * 
+ * (Code Template Version: 1.0)
+ */
+package br.facet.tcc.impl.dao;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.cfg.NotYetImplementedException;
+import org.springframework.stereotype.Repository;
+
+import br.facet.tcc.dao.Dao;
+import br.facet.tcc.pojo.Endereco;
+
+/**
+ * <code>EnderecoDaoImpl</code><br>
+ * 
+ * Implementação de uma Dao para a classe {@link Endereco}
+ * 
+ * @author Osnir F CUNHA
+ * 
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+@Repository("enderecoDao")
+public class EnderecoDaoImpl extends DaoConfiguration implements Dao<Endereco> {
+
+    /**
+     * @see br.facet.tcc.dao.Dao#salvar(java.lang.Object)
+     * @since since optional
+     */
+    @Override
+    public Integer salvar(Endereco t) throws HibernateException {
+        return (Integer) this.getHibernateTemplate().save(t);
+    }
+
+    /**
+     * @see br.facet.tcc.dao.Dao#atualizar(java.lang.Object)
+     * @since since optional
+     */
+    @Override
+    public void atualizar(Endereco t) {
+        this.getHibernateTemplate().update(t);
+    }
+
+    /**
+     * @see br.facet.tcc.dao.Dao#excluir(java.lang.Object)
+     * @since since optional
+     */
+    @Override
+    public void excluir(Endereco t) {
+        this.getHibernateTemplate().delete(t);
+    }
+
+    /**
+     * @see br.facet.tcc.dao.Dao#listar()
+     * @since since optional
+     */
+    @Override
+    public List<Endereco> listar() {
+        return this.getHibernateTemplate().find("from Endereco");
+    }
+
+    /**
+     * @see br.facet.tcc.dao.Dao#pesquisar(java.lang.Object)
+     * @since since optional
+     */
+    @Override
+    public List<Endereco> pesquisar(Endereco t) {
+        throw new NotYetImplementedException("Classe em desenvolvimento.");
+    }
+}
