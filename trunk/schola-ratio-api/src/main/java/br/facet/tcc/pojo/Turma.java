d3ad7b7f2f2cb05e0a40292852dbe26e64572632
osnircunha@gmail.com <osnircunha@gmail.com@d23f665e-650f-5060-e4bd-094653392495>
2012-08-13 20:57:42 +0000

    versão inicial da classe, faltando checar as anotacoes do hibernate

---
+++ trunk/schola-ratio-api/src/main/java/br/facet/tcc/pojo/Turma.java
@@ -0,0 +1,130 @@
+/*
+ * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
+ * 
+ * Copyright (c) 2012 All rights reserved.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system, nor translated in any human or computer
+ * language in any way for any purposes whatsoever without the prior written
+ * 
+ * 
+ * (Code Template Version: 1.0)
+ */
+package br.facet.tcc.pojo;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import br.facet.tcc.enums.Status;
+
+/**
+ * @author Osnir F CUNHA
+ * 
+ *         FIXME : add joins to get methods
+ * 
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+@Entity
+@Table(name = "tb_turma")
+@SequenceGenerator(name = "seq_turma", sequenceName = "seq_turma", allocationSize = 1)
+public class Turma {
+
+    private Integer id;
+
+    private Professor professor;
+
+    private Disciplina disciplina;
+
+    private Status status;
+
+    private List<Aluno> alunos;
+
+    /**
+     * @return the id
+     */
+    @Id
+    @GeneratedValue(generator = "seq_turma")
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @return the professor
+     */
+    @Column
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    /**
+     * @return the disciplina
+     */
+    @Column
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    /**
+     * @return the status
+     */
+    @Column
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * @return the alunos
+     */
+    @Column
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @param professor
+     *            the professor to set
+     */
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    /**
+     * @param disciplina
+     *            the disciplina to set
+     */
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    /**
+     * @param status
+     *            the status to set
+     */
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    /**
+     * @param alunos
+     *            the alunos to set
+     */
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+}
