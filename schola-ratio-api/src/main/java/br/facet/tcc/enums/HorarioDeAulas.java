9c3d5155880d77906d513ab2661db7c20db1be8e
osnircunha <osnircunha@gmail.com>
2012-08-13 17:56:18 +0000

    Versão inicial da enum

---
+++ schola-ratio-api/src/main/java/br/facet/tcc/enums/HorarioDeAulas.java
@@ -0,0 +1,28 @@
+/*
+ * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
+ * 
+ * Copyright (c) 2012 All rights reserved.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system, nor translated in any human or computer
+ * language in any way for any purposes whatsoever without the prior written
+ * 
+ * 
+ * (Code Template Version: 1.0)
+ */
+package br.facet.tcc.enums;
+
+/**
+ * @author Osnir F CUNHA
+ * 
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public enum HorarioDeAulas {
+    SEGUNDA_PRIMEIRO_HORARIO, SEGUNDA_SEGUNDO_HORARIO, 
+    TERCA_PRIMEIRO_HORARIO, TERCA_SEGUNDO_HORARIO, 
+    QUARTA_PRIMEIRO_HORARIO, QUARTA_SEGUNDO_HORARIO, 
+    QUINTA_PRIMEIRO_HORARIO, QUINTA_SEGUNDO_HORARIO, 
+    SEXTA_PRIMEIRO_HORARIO, SEXTA_SEGUNDO_HORARIO
+}
