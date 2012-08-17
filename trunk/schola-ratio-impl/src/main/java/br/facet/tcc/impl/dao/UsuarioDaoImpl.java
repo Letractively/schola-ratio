ffd6356155d31694e20a737406c9519d81eebb47
osnircunha@gmail.com <osnircunha@gmail.com@d23f665e-650f-5060-e4bd-094653392495>
2012-08-17 23:32:14 +0000

    Atualizado para classe generica.

--- trunk/schola-ratio-impl/src/main/java/br/facet/tcc/impl/dao/UsuarioDaoImpl.java
+++ trunk/schola-ratio-impl/src/main/java/br/facet/tcc/impl/dao/UsuarioDaoImpl.java
@@ -13,12 +13,8 @@
  */
 package br.facet.tcc.impl.dao;
 
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
-import org.hibernate.Criteria;
-
-import br.facet.tcc.dao.Dao;
-import br.facet.tcc.impl.util.HibernateUtil;
 import br.facet.tcc.pojo.Usuario;
 
 /**
@@ -27,56 +23,7 @@ import br.facet.tcc.pojo.Usuario;
  * @version 0.0.1
  * @since 0.0.1
  */
-public class UsuarioDaoImpl extends DaoConfiguration implements Dao<Usuario> {
-
-    /**
-     * @see br.facet.tcc.dao.Dao#salvar(java.lang.Object)
-     * @since since optional
-     */
-    @Override
-    public Integer salvar(Usuario t) {
-
-        Integer id = (Integer) getHibernateTemplate().save(t);
-
-        return id;
-    }
-
-    /**
-     * @see br.facet.tcc.dao.Dao#atualizar(java.lang.Object)
-     * @since since optional
-     */
-    @Override
-    public void atualizar(Usuario t) {
-        getHibernateTemplate().update(t);
-    }
-
-    /**
-     * @see br.facet.tcc.dao.Dao#excluir(java.lang.Object)
-     * @since since optional
-     */
-    @Override
-    public void excluir(Usuario t) {
-        getHibernateTemplate().delete(t);
-    }
-
-    /**
-     * @see br.facet.tcc.dao.Dao#listar()
-     * @since since optional
-     */
-    @Override
-    public List<Usuario> listar() {
-        return getHibernateTemplate().find("from Usuario");
-    }
-
-    /**
-     * @see br.facet.tcc.dao.Dao#pesquisar(java.lang.Object)
-     * @since since optional
-     */
-    @Override
-    public List<Usuario> pesquisar(Usuario t) {
-        Criteria criteria = HibernateUtil.createCriteria(t, getSession());
-
-        return criteria.list();
-    }
+@Repository("usuarioDao")
+public class UsuarioDaoImpl extends DaoConfiguration<Usuario> {
 
 }
