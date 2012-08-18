6032db013128123c3cb79d9838147f5b41f0f6f9
osnircunha <osnircunha@gmail.com>
2012-08-18 21:13:38 +0000

    alterado consistencia dos atributos

--- schola-ratio-api/src/main/java/br/facet/tcc/enums/Situacao.java
+++ schola-ratio-api/src/main/java/br/facet/tcc/enums/Situacao.java
@@ -1,9 +1,8 @@
 /*
  * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
- *
- * Copyright (c) 2012
- * All rights reserved.
- *
+ * 
+ * Copyright (c) 2012 All rights reserved.
+ * 
  * This software is only to be used for the purpose for which it has been
  * provided. No part of it is to be reproduced, disassembled, transmitted,
  * stored in a retrieval system, nor translated in any human or computer
@@ -22,38 +21,6 @@ package br.facet.tcc.enums;
  */
 public enum Situacao {
 
-    APROVADO(1), REPROVADO(2);
-
-    private int codigo;
-
-    /**
-     * Construtor unico e privado que cria uma enum Situação apartir de um
-     * codigo.
-     * 
-     * @param codigo
-     *            Valor que sera armazenado para definir o Status
-     * @since 0.0.1
-     */
-    private Situacao(int codigo) {
-        this.codigo = codigo;
-    }
-
-    public static Situacao fromInt(int codigo) {
-        switch (codigo) {
-        case 1:
-            return APROVADO;
-        case 2:
-            return REPROVADO;
-        default:
-            return null;
-        }
-    }
-
-    /**
-     * @return the codigo
-     */
-    public int getCodigo() {
-        return codigo;
-    }
+    APROVADO, REPROVADO
 
 }
