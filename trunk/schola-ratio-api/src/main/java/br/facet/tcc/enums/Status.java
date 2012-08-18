472da726f48f055fafecbc34a3c81159c5af8570
osnircunha@gmail.com <osnircunha@gmail.com@d23f665e-650f-5060-e4bd-094653392495>
2012-08-18 21:13:38 +0000

    alterado consistencia dos atributos

--- trunk/schola-ratio-api/src/main/java/br/facet/tcc/enums/Status.java
+++ trunk/schola-ratio-api/src/main/java/br/facet/tcc/enums/Status.java
@@ -1,9 +1,8 @@
 /*
  * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
- *
- * Copyright (c) 2012
- * All rights reserved.
- *
+ * 
+ * Copyright (c) 2012 All rights reserved.
+ * 
  * This software is only to be used for the purpose for which it has been
  * provided. No part of it is to be reproduced, disassembled, transmitted,
  * stored in a retrieval system, nor translated in any human or computer
@@ -22,44 +21,5 @@ package br.facet.tcc.enums;
  */
 public enum Status {
 
-    ATIVO(1), INATIVO(2);
-
-    private int codigo;
-
-    /**
-     * @param codigo
-     *            Valor que sera armazenado para definir o Status
-     * @since 0.0.1
-     */
-    private Status(int codigo) {
-        this.codigo = codigo;
-    }
-
-    /**
-     * Converte um codigo inteiro para um Enum do tipo Status.
-     * 
-     * @param codigo
-     *            valor armazenado que representa um Status.
-     * @return Constante enum para o codigo passado.
-     * @since 0.0.1
-     */
-    public static Status fromInt(int codigo) {
-        switch (codigo) {
-        case 1:
-            return ATIVO;
-        case 2:
-            return INATIVO;
-
-        default:
-            return null;
-        }
-    }
-
-    /**
-     * @return the codigo
-     */
-    public int getCodigo() {
-        return codigo;
-    }
-
+    ATIVO, INATIVO
 }
