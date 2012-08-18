1964f2a4f7095ad6e6ef75f0434edb7635ed5490
osnircunha@gmail.com <osnircunha@gmail.com@d23f665e-650f-5060-e4bd-094653392495>
2012-08-18 20:56:42 +0000

    adicionado os atributos das daos que estavam faltando.

--- trunk/schola-ratio-impl/src/test/java/br/facet/tcc/impl/dao/test/DaoTestCaseSetUp.java
+++ trunk/schola-ratio-impl/src/test/java/br/facet/tcc/impl/dao/test/DaoTestCaseSetUp.java
@@ -5,14 +5,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import br.facet.tcc.impl.dao.AlunoCursoDaoImpl;
+import br.facet.tcc.impl.dao.AlunoDaoImpl;
+import br.facet.tcc.impl.dao.AvaLiacaoDeAlunoDaoImpl;
+import br.facet.tcc.impl.dao.AvaliacaoDaoImpl;
+import br.facet.tcc.impl.dao.CursoDaoImpl;
+import br.facet.tcc.impl.dao.DescontoDaoImpl;
+import br.facet.tcc.impl.dao.DisciplinaDaoImpl;
 import br.facet.tcc.impl.dao.EnderecoDaoImpl;
+import br.facet.tcc.impl.dao.InstituicaoDaoImpl;
+import br.facet.tcc.impl.dao.MensalidadeDaoImpl;
+import br.facet.tcc.impl.dao.PermissaoDaoImpl;
+import br.facet.tcc.impl.dao.ProfessorDaoImpl;
+import br.facet.tcc.impl.dao.SituacaoDoAlunoNaTurmaDaoImpl;
+import br.facet.tcc.impl.dao.TurmaDaoImpl;
+import br.facet.tcc.impl.dao.UsuarioDaoImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/applicationContext.xml"})
 public class DaoTestCaseSetUp {
 
     @Autowired
+    protected AlunoDaoImpl alunoDao;
+    @Autowired
+    protected AlunoCursoDaoImpl alunoCursoDao;
+    @Autowired
+    protected AvaliacaoDaoImpl avaliacaoDao;
+    @Autowired
+    protected AvaLiacaoDeAlunoDaoImpl avaLiacaoDeAlunoDao;
+    @Autowired
+    protected CursoDaoImpl cursoDao;
+    @Autowired
+    protected DescontoDaoImpl descontoDao;
+    @Autowired
+    protected DisciplinaDaoImpl disciplinaDao;
+    @Autowired
     protected EnderecoDaoImpl enderecoDao;
+    @Autowired
+    protected InstituicaoDaoImpl instituicaoDao;
+    @Autowired
+    protected MensalidadeDaoImpl mensalidadeDao;
+    @Autowired
+    protected PermissaoDaoImpl permissaoDao;
+    @Autowired
+    protected ProfessorDaoImpl professorDao;
+    @Autowired
+    protected SituacaoDoAlunoNaTurmaDaoImpl situacaoDoAlunoNaTurmaDao;
+    @Autowired
+    protected TurmaDaoImpl turmaDao;
+    @Autowired
+    protected UsuarioDaoImpl usuarioDao;
 
     /**
      * @param enderecoDao
@@ -20,6 +62,104 @@ public class DaoTestCaseSetUp {
      */
     public EnderecoDaoImpl getEnderecoDao() {
         return this.enderecoDao;
+    }
+
+    /**
+     * @return the alunoDao
+     */
+    public AlunoDaoImpl getAlunoDao() {
+        return alunoDao;
+    }
+
+    /**
+     * @return the avaliacaoDao
+     */
+    public AvaliacaoDaoImpl getAvaliacaoDao() {
+        return avaliacaoDao;
+    }
+
+    /**
+     * @return the avaLiacaoDeAlunoDao
+     */
+    public AvaLiacaoDeAlunoDaoImpl getAvaLiacaoDeAlunoDao() {
+        return avaLiacaoDeAlunoDao;
+    }
+
+    /**
+     * @return the cursoDao
+     */
+    public CursoDaoImpl getCursoDao() {
+        return cursoDao;
+    }
+
+    /**
+     * @return the disciplinaDao
+     */
+    public DisciplinaDaoImpl getDisciplinaDao() {
+        return disciplinaDao;
+    }
+
+    /**
+     * @return the instituicaoDao
+     */
+    public InstituicaoDaoImpl getInstituicaoDao() {
+        return instituicaoDao;
+    }
+
+    /**
+     * @return the mensalidadeDao
+     */
+    public MensalidadeDaoImpl getMensalidadeDao() {
+        return mensalidadeDao;
+    }
+
+    /**
+     * @return the professorDao
+     */
+    public ProfessorDaoImpl getProfessorDao() {
+        return professorDao;
+    }
+
+    /**
+     * @return the turmaDao
+     */
+    public TurmaDaoImpl getTurmaDao() {
+        return turmaDao;
+    }
+
+    /**
+     * @return the usuarioDao
+     */
+    public UsuarioDaoImpl getUsuarioDao() {
+        return usuarioDao;
+    }
+
+    /**
+     * @return the alunoCursoDao
+     */
+    public AlunoCursoDaoImpl getAlunoCursoDao() {
+        return alunoCursoDao;
+    }
+
+    /**
+     * @return the descontoDao
+     */
+    public DescontoDaoImpl getDescontoDao() {
+        return descontoDao;
+    }
+
+    /**
+     * @return the permissaoDao
+     */
+    public PermissaoDaoImpl getPermissaoDao() {
+        return permissaoDao;
+    }
+
+    /**
+     * @return the situacaoDoAlunoNaTurmaDao
+     */
+    public SituacaoDoAlunoNaTurmaDaoImpl getSituacaoDoAlunoNaTurmaDao() {
+        return situacaoDoAlunoNaTurmaDao;
     }
 
 }
