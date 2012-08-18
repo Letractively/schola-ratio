434cdfede30071441b9bf11e6e3c47d82d2010fb
osnircunha@gmail.com <osnircunha@gmail.com@d23f665e-650f-5060-e4bd-094653392495>
2012-08-18 18:54:07 +0000

    orgaanizado os imports

--- trunk/schola-ratio-api/src/main/java/br/facet/tcc/pojo/Disciplina.java
+++ trunk/schola-ratio-api/src/main/java/br/facet/tcc/pojo/Disciplina.java
@@ -23,7 +23,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
