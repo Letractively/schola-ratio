3d5e6e71b431baacc2b277d668349e2fff1d4213
osnircunha <osnircunha@gmail.com>
2012-08-18 00:12:03 +0000

    versão inicial da classe

---
+++ schola-ratio-impl/src/main/java/br/facet/tcc/impl/dao/AvaliacaoDaoImpl.java
@@ -0,0 +1,29 @@
+/*
+ * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
+ * 
+ * Copyright (c) 2012 All rights reserved.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system, nor translated in any human or computer
+ * language in any way for any purposes whatsoever without the prior written
+ * 
+ * 
+ * (Code Template Version: 1.0)
+ */
+package br.facet.tcc.impl.dao;
+
+import org.springframework.stereotype.Repository;
+
+import br.facet.tcc.pojo.Avaliacao;
+
+/**
+ * @author Osnir F CUNHA
+ * 
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+@Repository("avaliacaoDao")
+public class AvaliacaoDaoImpl extends DaoConfiguration<Avaliacao> {
+
+}
