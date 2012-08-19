5a8fdf7bc7f6eddabe5a2e289fd0cfc5a3c46e8c
djulles1@gmail.com <djulles1@gmail.com@d23f665e-650f-5060-e4bd-094653392495>
2012-08-19 05:23:46 +0000

    Versao inicial da classe

---
+++ trunk/schola-ratio-impl/src/test/java/br/facet/tcc/impl/dao/test/DisciplinaDaoImplTest.java
@@ -0,0 +1,87 @@
+
+ /*
+ * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
+ *
+ * Copyright (c) 2012
+ * All rights reserved.
+ *
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system, nor translated in any human or computer
+ * language in any way for any purposes whatsoever without the prior written
+ * 
+ * 
+ * (Code Template Version: 1.0)
+ */
+package br.facet.tcc.impl.dao.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+ /**
+ * @author Djulles IKEDA
+ *
+ * @version TODO: class_version
+ * @since TODO: package_version 
+ */
+public class DisciplinaDaoImplTest extends DaoTestCaseSetUp {
+
+    /**
+     * Test method for {@link br.facet.tcc.impl.dao.DaoConfiguration#salvar(java.lang.Object)}.
+     */
+    @Test
+    public final void testSalvar() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link br.facet.tcc.impl.dao.DaoConfiguration#atualizar(java.lang.Object)}.
+     */
+    @Test
+    public final void testAtualizar() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link br.facet.tcc.impl.dao.DaoConfiguration#excluir(java.lang.Object)}.
+     */
+    @Test
+    public final void testExcluir() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link br.facet.tcc.impl.dao.DaoConfiguration#listar(java.lang.Class)}.
+     */
+    @Test
+    public final void testListar() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link br.facet.tcc.impl.dao.DaoConfiguration#pesquisar(java.lang.Object)}.
+     */
+    @Test
+    public final void testPesquisar() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link br.facet.tcc.impl.dao.DaoConfiguration#obterPorID(java.lang.Class, java.lang.Integer)}.
+     */
+    @Test
+    public final void testObterPorID() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link br.facet.tcc.impl.dao.DaoConfiguration#addSessionFactoryToDao(org.hibernate.SessionFactory)}.
+     */
+    @Test
+    public final void testAddSessionFactoryToDao() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
