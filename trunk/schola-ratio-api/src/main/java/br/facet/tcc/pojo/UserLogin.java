2727981d079b244a0989b427d93190f68b895e1d
osnircunha@gmail.com <osnircunha@gmail.com@d23f665e-650f-5060-e4bd-094653392495>
2012-08-26 22:35:50 +0000

    Versão inicial

---
+++ trunk/schola-ratio-api/src/main/java/br/facet/tcc/pojo/UserLogin.java
@@ -0,0 +1,107 @@
+/*
+ * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
+ * 
+ * Copyright (c) 2012 All rights reserved.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system, nor translated in any human or computer
+ * language in any way for any purposes whatsoever without the prior written
+ * 
+ * 
+ * (Code Template Version: 1.0)
+ */
+package br.facet.tcc.pojo;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * @author Osnir F CUNHA
+ * 
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+@Entity
+@Table(name = "tb_user_login")
+public class UserLogin {
+
+    private String username;
+
+    private String password;
+
+    private boolean enable;
+
+    private List<Permissao> permissoes;
+
+    /**
+     * @return the username
+     */
+    @Id
+    @Column(name = "user_name")
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @return the password
+     */
+    @Column
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @return the enable
+     */
+    @Column(columnDefinition = "BOOLEAN")
+    public boolean isEnable() {
+        return enable;
+    }
+
+    /**
+     * @return the permissoes
+     */
+    @OneToMany
+    public List<Permissao> getPermissoes() {
+        return permissoes;
+    }
+
+    /**
+     * @param username
+     *            the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @param password
+     *            the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @param enable
+     *            the enable to set
+     */
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    /**
+     * @param permissoes
+     *            the permissoes to set
+     */
+    public void setPermissoes(List<Permissao> permissoes) {
+        this.permissoes = permissoes;
+    }
+
+}
