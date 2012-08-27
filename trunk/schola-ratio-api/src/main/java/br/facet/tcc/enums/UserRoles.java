761cb4092b233447ccedcda35dddbfeb488016cd
osnircunha@gmail.com <osnircunha@gmail.com@d23f665e-650f-5060-e4bd-094653392495>
2012-08-27 00:07:29 +0000

    Versão inicial

---
+++ trunk/schola-ratio-api/src/main/java/br/facet/tcc/enums/UserRoles.java
@@ -0,0 +1,50 @@
+/*
+ * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
+ * 
+ * Copyright (c) 2012 All rights reserved.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system, nor translated in any human or computer
+ * language in any way for any purposes whatsoever without the prior written
+ * 
+ * 
+ * (Code Template Version: 1.0)
+ */
+package br.facet.tcc.enums;
+
+/**
+ * Define as roles que serão utilizadas para controle de acesso.
+ * 
+ * @author Osnir F CUNHA
+ * 
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public enum UserRoles {
+
+    /**
+     * Academico
+     */
+    ROLE_ACA,
+
+    /**
+     * Administrativo
+     */
+    ROLE_ADM,
+
+    /**
+     * Configuração
+     */
+    ROLE_CFG,
+
+    /**
+     * Financeiro
+     */
+    ROLE_FIN,
+
+    /**
+     * Usuarios
+     */
+    ROLE_USR
+}
