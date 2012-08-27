a5c4f9907343b01985ca7288d16ca991692df606
osnircunha <osnircunha@gmail.com>
2012-08-27 01:10:39 +0000

    corrigido alguns inserts

--- schola-ratio-impl/src/test/resources/import.sql
+++ schola-ratio-impl/src/test/resources/import.sql
@@ -3,17 +3,15 @@ insert into tb_endereco (bairro, cep, cidade, complemento, estado, numero, rua, 
 insert into tb_endereco (bairro, cep, cidade, complemento, estado, numero, rua, id_endereco) values ('AV', 98769809, 'CTBA', '', 'PR', 500,'Praça rosa', nextval('seq_endereco'));
 
 
-inserto into tb_
-
 INSERT INTO tb_instituicao(id_instituicao, cnpj, email_instituicao, inscricao_estadual, nome_instituicao, telefone_instituicao, endereco_instituicao) VALUES (nextval('seq_instituicao'), 123456789, 'aaa@aaa.com', 12345, 'ScholaRatio1', 12345234, 1);
 INSERT INTO tb_instituicao(id_instituicao, cnpj, email_instituicao, inscricao_estadual, nome_instituicao, telefone_instituicao, endereco_instituicao) VALUES (nextval('seq_instituicao'), 234567890, 'bbb@bbb.com', 23456, 'ScholaRatio2', 23452345, 2);
 INSERT INTO tb_instituicao(id_instituicao, cnpj, email_instituicao, inscricao_estadual, nome_instituicao, telefone_instituicao, endereco_instituicao) VALUES (nextval('seq_instituicao'), 345678901, 'ccc@ccc.com', 12345, 'ScholaRatio3', 34563456, 3);
 
 
-insert into tb_aluno (aluno_id, cpf, dataExpedicao, email, endereco_usuario, nacionalidade, naturalidade, nome, nomeMae, nomePai, orgaoExpeditor, rg, senha, sexo, status, ufOrgaoExpeditor, dataDeMatricula) values (nextval('seq_aluno'),'33333333333','2003-08-15','wwwrrr@email.com','1','Brasileiro','São Vicente','Astolpho P Cruz','','','SSP-SP','2222222222','123qwe','M','ATIVO','SP','2012-08-18');
-insert into tb_aluno (aluno_id, cpf, dataExpedicao, email, endereco_usuario, nacionalidade, naturalidade, nome, nomeMae, nomePai, orgaoExpeditor, rg, senha, sexo, status, ufOrgaoExpeditor, dataDeMatricula) values (nextval('seq_aluno'),'55555555555','1994-06-10','xxxxxx@email.com','2','Canadense','Curitiba','Josepha P Silva','','','SSP-SP','11111111','123qwe','F','ATIVO','SP','2012-08-18');
-insert into tb_aluno (aluno_id, cpf, dataExpedicao, email, endereco_usuario, nacionalidade, naturalidade, nome, nomeMae, nomePai, orgaoExpeditor, rg, senha, sexo, status, ufOrgaoExpeditor, dataDeMatricula) values (nextval('seq_aluno'),'88888888888','1999-04-01','oooooo@email.com','3','Brasileiro','São José dos Pinhais','Jeronimo Pires','','','SSP-RS','54387621','123qwe','M','ATIVO','RS','2012-08-18');
-insert into tb_aluno (aluno_id, cpf, dataExpedicao, email, endereco_usuario, nacionalidade, naturalidade, nome, nomeMae, nomePai, orgaoExpeditor, rg, senha, sexo, status, ufOrgaoExpeditor, dataDeMatricula) values (nextval('seq_aluno'),'44444444444','1994-04-01','444444@email.com','3','Brasileiro','São José dos Pinhais','Jeronimo Pires','','','SSP-RS','44387621','123qwe','M','ATIVO','RS','2012-08-14');
+insert into tb_aluno (aluno_id, cpf, dataExpedicao, email, endereco_usuario, nacionalidade, naturalidade, nome, nomeMae, nomePai, orgaoExpeditor, rg, sexo, status, ufOrgaoExpeditor, dataDeMatricula) values (nextval('seq_aluno'),'33333333333','2003-08-15','wwwrrr@email.com','1','Brasileiro','São Vicente','Astolpho P Cruz','','','SSP-SP','2222222222','M','ATIVO','SP','2012-08-18');
+insert into tb_aluno (aluno_id, cpf, dataExpedicao, email, endereco_usuario, nacionalidade, naturalidade, nome, nomeMae, nomePai, orgaoExpeditor, rg,  sexo, status, ufOrgaoExpeditor, dataDeMatricula) values (nextval('seq_aluno'),'55555555555','1994-06-10','xxxxxx@email.com','2','Canadense','Curitiba','Josepha P Silva','','','SSP-SP','11111111','F','ATIVO','SP','2012-08-18');
+insert into tb_aluno (aluno_id, cpf, dataExpedicao, email, endereco_usuario, nacionalidade, naturalidade, nome, nomeMae, nomePai, orgaoExpeditor, rg,  sexo, status, ufOrgaoExpeditor, dataDeMatricula) values (nextval('seq_aluno'),'88888888888','1999-04-01','oooooo@email.com','3','Brasileiro','São José dos Pinhais','Jeronimo Pires','','','SSP-RS','54387621','M','ATIVO','RS','2012-08-18');
+insert into tb_aluno (aluno_id, cpf, dataExpedicao, email, endereco_usuario, nacionalidade, naturalidade, nome, nomeMae, nomePai, orgaoExpeditor, rg,  sexo, status, ufOrgaoExpeditor, dataDeMatricula) values (nextval('seq_aluno'),'44444444444','1994-04-01','444444@email.com','3','Brasileiro','São José dos Pinhais','Jeronimo Pires','','','SSP-RS','44387621','M','ATIVO','RS','2012-08-14');
 
 
 insert into tb_user_login( user_name, enable, password) values ( 'user_name1', 'TRUE', 'senha');
@@ -23,6 +21,7 @@ insert into tb_user_login( user_name, enable, password) values ( 'user_name4', '
 insert into tb_user_login( user_name, enable, password) values ( 'user_name5', 'TRUE', 'senha');
 insert into tb_user_login( user_name, enable, password) values ( 'user_name6', 'TRUE', 'senha');
 insert into tb_user_login( user_name, enable, password) values ( 'user_name7', 'TRUE', 'senha');
+insert into tb_user_login( user_name, enable, password) values ( 'user_name8', 'TRUE', 'senha');
 
 
 INSERT INTO tb_permissao( id, role, username) VALUES (nextval('hibernate_sequence'), 'ROLE_FIN', 'user_name1');
