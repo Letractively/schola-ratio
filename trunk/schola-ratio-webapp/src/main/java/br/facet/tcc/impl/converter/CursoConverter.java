6d2ea873f583e225fca07a918b9fb8aaf73b432b
osnircunha <osnircunha@d23f665e-650f-5060-e4bd-094653392495>
2012-11-10 20:16:33 +0000

    versão inicial da classe

---
+++ trunk/schola-ratio-webapp/src/main/java/br/facet/tcc/impl/converter/CursoConverter.java
@@ -0,0 +1,80 @@
+/*
+ * TCC Facet 2012 - Djulles IKEDA e Osnir F CUNHA.
+ *
+ * Copyright (c) 2012
+ * All rights reserved.
+ *
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system, nor translated in any human or computer
+ * language in any way for any purposes whatsoever without the prior written
+ * 
+ * 
+ * (Code Template Version: 1.0)
+ */
+package br.facet.tcc.impl.converter;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import br.facet.tcc.exception.ServiceException;
+import br.facet.tcc.impl.service.GestaoAdministrativoImpl;
+import br.facet.tcc.pojo.Curso;
+
+/**
+ * @author Osnir F CUNHA
+ * 
+ */
+@ManagedBean(name = "cursoConverter")
+@RequestScoped
+@FacesConverter("cursoConverter")
+public class CursoConverter implements Converter {
+
+    @ManagedProperty("#{gestaoAdministrativo}")
+    private GestaoAdministrativoImpl gestaoAdministrativo;
+
+    /**
+     * @see javax.faces.convert.Converter#getAsObject(javax.faces.context.FacesContext,
+     *      javax.faces.component.UIComponent, java.lang.String)
+     * @since since optional
+     */
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component,
+            String value) {
+        Curso curso = new Curso();
+        curso.setNome(value);
+
+        Curso retorno = null;
+
+        try {
+            retorno = this.gestaoAdministrativo.buscarCurso(curso).get(0);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+
+        return retorno;
+    }
+
+    /**
+     * @see javax.faces.convert.Converter#getAsString(javax.faces.context.FacesContext,
+     *      javax.faces.component.UIComponent, java.lang.Object)
+     * @since since optional
+     */
+    @Override
+    public String getAsString(FacesContext context, UIComponent component,
+            Object value) {
+        Curso curso = (Curso) value;
+        return curso.getNome();
+    }
+
+    public void setGestaoAdministrativo(
+            GestaoAdministrativoImpl gestaoAdministrativo) {
+        this.gestaoAdministrativo = gestaoAdministrativo;
+    }
+
+}
