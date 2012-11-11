7c2a088509c0352e7e62c8e9bdfb3f5f13c3b5a2
osnircunha <osnircunha@gmail.com>
2012-11-11 23:05:03 +0000

    adequação para a mudança de aluno p alunocurso

--- schola-ratio-impl/src/main/java/br/facet/tcc/impl/dao/AlunoCursoDaoImpl.java
+++ schola-ratio-impl/src/main/java/br/facet/tcc/impl/dao/AlunoCursoDaoImpl.java
@@ -14,9 +14,20 @@
  */
 package br.facet.tcc.impl.dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Conjunction;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.Subqueries;
 import org.springframework.stereotype.Repository;
 
+import br.facet.tcc.exception.DaoException;
+import br.facet.tcc.pojo.Aluno;
 import br.facet.tcc.pojo.AlunoCurso;
+import br.facet.tcc.pojo.UserLogin;
 
 /**
  * @author Djulles IKEDA
@@ -27,4 +38,45 @@ import br.facet.tcc.pojo.AlunoCurso;
 @Repository("alunoCursoDao")
 public class AlunoCursoDaoImpl extends DaoConfiguration<AlunoCurso> {
 
+    @Override
+    public List<AlunoCurso> pesquisar(AlunoCurso t) throws DaoException {
+        Criteria criteria = this.getSession().createCriteria(AlunoCurso.class,
+                "AC");
+
+        Conjunction aluno = Restrictions.conjunction();
+        Conjunction userLogin = Restrictions.conjunction();
+        if (t.getAluno() != null) {
+            aluno.add(Subqueries.exists(DetachedCriteria.forClass(Aluno.class)
+                    .setProjection(Projections.id())));
+            criteria.createAlias("AC.aluno", "a");
+            // Nome
+            if (!"".equals(t.getAluno().getNome())
+                    && t.getAluno().getNome() != null) {
+                aluno.add(Restrictions.ilike("a.nome", "%"
+                        + t.getAluno().getNome() + "%"));
+            }
+            // CPF
+            if (t.getAluno().getCpf() != null) {
+                aluno.add(Restrictions.eq("a.cpf", t.getAluno().getCpf()));
+            }
+            aluno.add(Restrictions.eq("a.status", t.getAluno().getStatus()));
+
+            aluno.add(Restrictions.eqProperty("AC.aluno.id", "a.id"));
+
+            if (t.getAluno().getUserLogin() != null
+                    && t.getAluno().getUserLogin().getUsername() != null) {
+                userLogin.add(Subqueries.exists(DetachedCriteria.forClass(
+                        UserLogin.class).setProjection(Projections.id())));
+                criteria.createAlias("a.userLogin", "ul");
+                userLogin.add(Restrictions.ilike("ul.username", "%"
+                        + t.getAluno().getUserLogin().getUsername() + "%"));
+                userLogin.add(Restrictions
+                        .eqProperty("a.userLogin.id", "ul.id"));
+            }
+
+            criteria.add(aluno.add(userLogin));
+        }
+
+        return criteria.list();
+    }
 }
